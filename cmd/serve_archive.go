8a7dc2f4b13c96cbe4b4de016a0c22cdd1e1c7e9
quobix <dave@quobix.com>
2026-05-30 07:39:40 -0400

    clean up experience a little

--- cmd/serve_archive.go
+++ cmd/serve_archive.go
@@ -8,13 +8,6 @@ import (
 
 type staticServerOptions = ppserve.Config
 
-func archiveExportURLForServe(opts *rootOptions) string {
-	if opts == nil || !opts.serve || opts.disableExport {
-		return ""
-	}
-	return ppserve.ArchiveExportPathForBaseURL(opts.baseURL)
-}
-
 func serveOutputDir(addr string, opts staticServerOptions) error {
 	return opts.ListenAndServe(context.Background(), addr)
 }
